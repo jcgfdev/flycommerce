605729e248ef77956e9d5925a8ebcd8d0d34a0af
jcgfdev <jcgf1406@gmail.com>
2025-06-17 08:09:15 -0500

    subida inicial del proyecto

---
+++ src/main/java/com/jcgfdev/flycommerce/FlycommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jcgfdev.flycommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlycommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlycommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jcgfdev/flycommerce/FlycommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcgfdev.flycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlycommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
